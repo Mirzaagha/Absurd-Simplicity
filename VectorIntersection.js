f440a7fd98396ccd25cdfa4d792f074ab5e3f0b8
Mirzaagha Guliyev <35248449+Mirzaagha@users.noreply.github.com>
2024-01-21 10:48:29 +0100

    Update VectorIntersection.js

--- VectorIntersection.js
+++ VectorIntersection.js
@@ -3,7 +3,9 @@ function Intersection(v,v2){
   var b1 = v[1] - v[0]*k1;
   var k2 = (v2[3]-v2[1])/(v2[2]-v2[0]);
   var b2 = v2[1] - v2[0]*k2;
-  var y,z,w,k1,b1;
-  
+  var x,y,z,w,k1,b1;
+  w = k1-k2;
+  z = b2-b1;
+  x = z/w;
   y = k1 * z/w - b1;
 }
